cdf0c3ddd3abdd4de74d32f37ae7201b9e99b4a3
hirentimbadiya <hirentimbadiya74@gmail.com>
2026-04-22 23:11:08 +0530

    feat: add description in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,14 @@
 output "sqs_queue_name" {
   value = module.base.sqs_queue_name
+  description = "SQS queue name"
 }
 
 output "step_scaling_service" {
   value = module.step_scaling.ecs_service_name
+  description = "ECS service name for step scaling"
 }
 
 output "target_tracking_service" {
   value = module.target_tracking.ecs_service_name
+  description = "ECS service name for target tracking scaling"
 }
